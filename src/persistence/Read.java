085bd15fed9fdd9111db10f3056a6e27b8478beb
Jay <pateljayn3@myvuw.ac.nz>
2019-09-16 14:58:53 +1200

    Json test in persistence

--- src/persistence/Read.java
+++ src/persistence/Read.java
@@ -1,7 +1,29 @@
 package persistence;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 /**
  * Read class for Chip's Challenge.
  */
 public class Read {
+
+    JSONParser parser = new JSONParser();
+
+    try{
+
+        Object obj = parser.parse(new FileReader("jsonTest.json"));
+        JSONObject jsonObj = (JSONObject) obj;
+
+    }
+    catch(FileNotFoundException e){ e.printStackTrace(); }
+    catch(IOException e){ e.printStackTrace(); }
+    catch(ParseException e){ e.printStackTrace(); }
+    catch(Exception e) { e.printStackTrace();}
+
 }
